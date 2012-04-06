70b53b4f73a28d5f9635a720aa24b47db6ecf755
Bagariavivek <vivekee047@gmail.com>
2012-04-06 16:40:39 +0530

    changed index

--- sql/WatchGroup.sql
+++ sql/WatchGroup.sql
@@ -4,28 +4,28 @@
 
  
 CREATE TABLE IF NOT EXISTS watchpages(
-	wp_id			INT unsigned		NOT NULL auto_increment PRIMARY KEY ,
-	wp_user			INT unsigned		NOT NULL ,		--	UserID ,
-	wp_title		VARCHAR(255)		NOT NULL ,		--	Title of the page
-	wp_groupname		VARCHAR(255)		NOT NULL ,		--	Name of the group
-	wp_namespace		INT unsigned		NOT NULL ,		--	Namespace number of the page
-	wp_notifytimestamp	VARCHAR(15)							
+	id			INT unsigned		NOT NULL auto_increment PRIMARY KEY ,
+	user			INT unsigned		NOT NULL ,		--	UserID ,
+	title		VARCHAR(255)		NOT NULL ,		--	Title of the page
+	groupname		VARCHAR(255)		NOT NULL ,		--	Name of the group
+	namespace		INT unsigned		NOT NULL ,		--	Namespace number of the page
+	notifytimestamp	VARCHAR(15)							
 );
 
-CREATE UNIQUE INDEX wp_id		ON watchpages(wp_id);
-CREATE UNIQUE INDEX wp_user		ON watchpages(wp_id);
-CREATE UNIQUE INDEX wp_title		ON watchpages(wp_id);
-CREATE UNIQUE INDEX wp_groupname	ON watchpages(wp_id);
-CREATE UNIQUE INDEX wp_namespace	ON watchpages(wp_id);
-CREATE UNIQUE INDEX wp_notifytimestamp	ON watchpages(wp_id);
+CREATE UNIQUE INDEX wp_id		ON watchpages(id);
+CREATE UNIQUE INDEX wp_user		ON watchpages(user);
+CREATE UNIQUE INDEX wp_title		ON watchpages(title);
+CREATE UNIQUE INDEX wp_groupname	ON watchpages(groupname);
+CREATE UNIQUE INDEX wp_namespace	ON watchpages(namespace);
+CREATE UNIQUE INDEX wp_notifytimestamp	ON watchpages(notifytimestamp);
 
 
 CREATE TABLE IF NOT EXISTS watchgroups (
-	wtg_id			INT unsigned		NOT NULL auto_increment PRIMARY KEY ,
-	wtg_user		INT unsigned		NOT NULL ,		--	UserID
-	wtg_groupname		VARCHAR(255)		NOT NULL ,		--	Name of the group
-	wtg_visible_group	SMALLINT unsigned	NOT NULL default 0,	--	Boolean - Can be viewed by any user
-	wtg_public_editable	SMALLINT unsigned	NOT NULL default 0	--	Boolean - Can be edited by any user
+	id			INT unsigned		NOT NULL auto_increment PRIMARY KEY ,
+	user		INT unsigned		NOT NULL ,		--	UserID
+	groupname		VARCHAR(255)		NOT NULL ,		--	Name of the group
+	visible_group	SMALLINT unsigned	NOT NULL default 0,	--	Boolean - Can be viewed by any user
+	public_editable	SMALLINT unsigned	NOT NULL default 0	--	Boolean - Can be edited by any user
 /*Yet to include more columns for preferences of each watchgroup*/
 );
 
